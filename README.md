7075c33350b5399c2114c3adf19e2d298170fda5
markus <datamastery87@gmail.com>
2023-01-20 16:59:55 +0100

    first try

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Svelte-and-PocketBase
-# Svelte-and-PocketBase
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center">
+		<h1>Test</h1>
+	</div>
+</div>
