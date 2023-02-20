6ec1642291f655b9499b277e10069a0ed4e59095
markus <datamastery87@gmail.com>
2023-02-20 19:07:51 +0100

    fix readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Svelte-and-PocketBase
+# Personal Website
+
+## Techstack
+
+### Frontend
+
+Svelte(Kit) & Tailwind
+
+### Backend
+
+FastAPI + Postgresql
+
+### Hosting
+
+Deployed mit Docker auf Digitalocean
--- frontend/src/components/Card.svelte
+++ frontend/src/components/Card.svelte
@@ -7,7 +7,7 @@
 <a target="_blank" rel="noreferrer" href={link}>
 	<div class="card w-full bg-base-100 shadow-xl">
 		<figure><img src={video_id} alt="Shoes" /></figure>
-		<div class="card-body text h-80">
+		<div class="card-body text md:h-80">
 			<h3 class="card-title pt-4 pb-10">{title}</h3>
 			<slot name="description" />
 		</div>
